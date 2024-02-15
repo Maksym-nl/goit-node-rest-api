14563e5219fa55230ec7a36ed9a92c87524f1e64
Maksym-nl <119403118+Maksym-nl@users.noreply.github.com>
2024-02-15 13:18:29 +0200

    на проверку

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import controller from "../controllers/usersControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import userSchema from "../schemas/userSchemas.js";
+import authenticate from "../middlewares/authenticate.js";
 const router = express.Router();
 
 router.post(
@@ -11,4 +12,6 @@ router.post(
 );
 
 router.post("/login", validateBody(userSchema.userSchema), controller.login);
+router.post("/logout", authenticate, controller.logout);
+router.get("/current", authenticate, controller.current);
 export default router;
--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -1,24 +1,8 @@
 import Joi from "joi";
 
 const userSchema = Joi.object({
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: {
-    type: String,
-    default: null,
-  },
+  password: Joi.string().required(),
+  email: Joi.string().required(),
 });
 
 const data = {
